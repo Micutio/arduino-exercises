ad606e9c898b6488522f0fb2eead1c2e959d55f6
Michael Wagner <wagner.mchl@googlemail.com>
2019-01-04 00:31:56 +0800

    Changed: Symbols can now transition in left direction across the dot matrix

--- dot-matrix-display/dot-matrix-display.ino
+++ dot-matrix-display/dot-matrix-display.ino
@@ -3,6 +3,7 @@
 
 //We always have to include the library
 #include "LedControl.h"
+#include "dm-symbols.h"
 
 /*
  Now we need a LedControl to work with.
@@ -15,123 +16,24 @@
 LedControl lc = LedControl(12,10,11,1);
 
 /* we always wait a bit between updates of the display */
-unsigned long delaytime1 = 950;
+unsigned long delaytime1 = 100;
 unsigned long delaytime2 = 1250;
-unsigned long delaytime3 = 150;
+unsigned long delaytime3 = 250;
+
+byte currentSymbol[8] = {};
 
 void setup() {
-  /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
-   */
-  lc.shutdown(0, false);
-  /* Set the brightness to a medium values */
-  lc.setIntensity(0, 8);
-  /* and clear the display */
-  lc.clearDisplay(0);
+    /*
+    The MAX72XX is in power-saving mode on startup,
+    we have to do a wakeup call
+    */
+    lc.shutdown(0, false);
+    /* Set the brightness to a medium values */
+    lc.setIntensity(0, 8);
+    /* and clear the display */
+    lc.clearDisplay(0);
 }
 
-/* here is the data for the characters */
-byte a[5] = {B00011110,
-             B00101010,
-             B00101010,
-             B00101010,
-             B00100100};
-byte A[5] = {B01111110,
-             B10001000,
-             B10001000,
-             B10001000,
-             B01111110};
-byte c[5] = {B00010010,
-             B00100010,
-             B00100010,
-             B00100010,
-             B00011100};
-byte C[5] = {B01000100,
-             B10000010,
-             B10000010,
-             B10000010,
-             B01111100};
-byte d[5] = {B11111110,
-             B00010010,
-             B00100010,
-             B00100010,
-             B00011100};
-byte e[5] = {B00011010,
-             B00101010,
-             B00101010,
-             B00101010,
-             B00011100};
-byte h[5] = {B00011110,
-             B00100000,
-             B00100000,
-             B00010000,
-             B11111110};
-byte i[5] = {B00000000,
-             B00000010,
-             B10111110,
-             B00100010,
-             B00000000};
-byte l[5] = {B00000010,
-             B00000010,
-             B01111100,
-             B10000000,
-             B00000000};
-byte L[5] = {B00000010,
-             B00000010,
-             B00000010,
-             B11111100,
-             B10000000};
-byte M[5] = {B01111110,
-             B10000000,
-             B01111000,
-             B10000000,
-             B01111110};           
-byte n[5] = {B00011110,
-             B00100000,
-             B00100000,
-             B00010000,
-             B00111110};
-byte o[5] = {B00011100,
-             B00100010,
-             B00100010,
-             B00100010,
-             B00011100};
-byte r[5] = {B00010000,
-             B00100000,
-             B00100000,
-             B00010000,
-             B00111110};
-byte u[5] = {B00111110,
-             B00000100,
-             B00000010,
-             B00000010,
-             B00111100};
-byte y[5] = {B00111100,
-             B00001010,
-             B00001010,
-             B00001010,
-             B00110010};
-byte plus[5] = {B00001000,
-                B00001000,
-                B00111110,
-                B00001000,
-                B00001000};
-byte eqls[5] = {B00010100,
-                B00010100,
-                B00010100,
-                B00010100,
-                B00010100};
-byte heart[5] = {B00110000,
-                 B01001000,
-                 B00100100,
-                 B01001000,
-                 B00110000};
-byte space[5] = {B00000000,
-                 B00000000,
-                 B00000000,
-                 B00000000,
-                 B00000000};
 
 /**
  * TODO: Change this method to allow for transition of symbols, e.g.: a marquee
@@ -144,12 +46,40 @@ byte space[5] = {B00000000,
         - copy bit 7-i from next symbol into current symbol 
  *  
  */
-void setLetter(byte lttr[5]) {
-  lc.setRow(0, 0, lttr[0]);
-  lc.setRow(0, 1, lttr[1]);
-  lc.setRow(0, 2, lttr[2]);
-  lc.setRow(0, 3, lttr[3]);
-  lc.setRow(0, 4, lttr[4]);
+
+
+void setLetter(byte lttr[], unsigned long delaytime) {
+    for (int i=0; i<5; ++i) {
+        currentSymbol[i] = lttr[i];
+    }
+    for (int i=0; i<8; ++i) {
+        lc.setRow(0, i, currentSymbol[i]);
+    }
+    delay(delaytime);
+}
+
+/**
+ * Sets a new symbol to transition to, by moving the letter towards the left.  
+ */
+void transitionToSymbol(byte lttr[5], unsigned long delayT) {
+    int lPtr = 4;
+    for (int i=0; i<8; ++i) {
+        for (int j=7; j>0; --j) {
+            currentSymbol[j] = currentSymbol[j-1];
+        }
+
+        if (lPtr >= 0) {
+            currentSymbol[1] = lttr[lPtr];
+        }
+        lPtr -= 1;
+        
+        // Display the new symbol.
+        for (int i=0; i<8; ++i) {
+            lc.setRow(0, i, currentSymbol[i]);
+        }
+        delay(delayT);
+    }
+    
 }
 
 /*
@@ -162,65 +92,33 @@ void setLetter(byte lttr[5]) {
 void writeMessageOnMatrix() {
 
   /* now display them one by one with a small delay */
-  
-  setLetter(C);
-  delay(delaytime1);
-  setLetter(h);
-  delay(delaytime1);
-  setLetter(i);
-  delay(delaytime1);
-  setLetter(n);
-  delay(delaytime2);
-  setLetter(L);
-  delay(delaytime1);
-  setLetter(e);
-  delay(delaytime1);
-  setLetter(space);
-  delay(delaytime3);
-  setLetter(e);
-  delay(delaytime2);
-  setLetter(plus);
-  delay(delaytime2);
-  setLetter(M);
-  delay(delaytime1);
-  setLetter(i);
-  delay(delaytime1);
-  setLetter(c);
-  delay(delaytime1);
-  setLetter(h);
-  delay(delaytime1);
-  setLetter(a);
-  delay(delaytime1);
-  setLetter(e);
-  delay(delaytime1);
-  setLetter(l);
-  delay(delaytime2);
-  setLetter(eqls);
-  delay(delaytime2);
-  setLetter(A);
-  delay(delaytime1);
-  setLetter(n);
-  delay(delaytime1);
-  setLetter(y);
-  delay(delaytime1);
-  setLetter(a);
-  delay(delaytime2);
-  setLetter(heart);
-  delay(delaytime2);
-  setLetter(space);
-  delay(delaytime3);
-  setLetter(heart);
-  delay(delaytime2);
-  setLetter(space);
-  delay(delaytime3);
-  setLetter(heart);
-  delay(delaytime2);
-  setLetter(space);
-  delay(delaytime3);
-  setLetter(heart);
-  delay(delaytime2);
-  setLetter(space);
-  delay(delaytime3);
+    transitionToSymbol(sym_C, delaytime1);
+    transitionToSymbol(sym_h, delaytime1);
+    transitionToSymbol(sym_i, delaytime1);
+    transitionToSymbol(sym_n, delaytime1);
+    transitionToSymbol(sym_L, delaytime1);
+    transitionToSymbol(sym_e, delaytime1);
+    transitionToSymbol(sym_e, delaytime1);
+    transitionToSymbol(sym_plus, delaytime1);
+    transitionToSymbol(sym_M, delaytime1);
+    transitionToSymbol(sym_i, delaytime1);
+    transitionToSymbol(sym_c, delaytime1);
+    transitionToSymbol(sym_h, delaytime1);
+    transitionToSymbol(sym_a, delaytime1);
+    transitionToSymbol(sym_e, delaytime1);
+    transitionToSymbol(sym_l, delaytime1);
+    transitionToSymbol(sym_eqls, delaytime1);
+    transitionToSymbol(sym_A, delaytime1);
+    transitionToSymbol(sym_n, delaytime1);
+    transitionToSymbol(sym_y, delaytime1);
+    transitionToSymbol(sym_a, delaytime1);
+    transitionToSymbol(sym_blank, delaytime3);
+    setLetter(sym_heart, delaytime2);
+    setLetter(sym_space, delaytime3);
+    setLetter(sym_heart, delaytime2);
+    setLetter(sym_space, delaytime3);
+    setLetter(sym_heart, delaytime2);
+    setLetter(sym_space, delaytime3);
 }
 
 /*
